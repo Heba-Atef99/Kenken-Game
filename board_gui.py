74c02a9c270e68640f45d07836a1adc539a691df
Heba Atef <hoba30.ha@gmail.com>
2022-05-27 00:15:07 +0200

    now we can draw a grid of any size

---
+++ board_gui.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from PIL import Image,ImageTk
+from tkinter import Toplevel, messagebox
+
+length = 80
+side_margin = 50
+top_margin = 80
+
+def create_grid(canvas, board_size):
+    top_margin = 90
+    left_margin = side_margin
+    for row in range(board_size):
+        r_start = top_margin + row * length
+        r_end = top_margin + (row+1) * length
+        for i in range(board_size):
+            start = left_margin + i * length
+            end = left_margin + (i+1) * length
+            canvas.create_rectangle(start, r_start, end, r_end, outline="black", fill="white")
+            offset = 7
+            canvas.create_text(3 + offset + start, offset + r_start, text = '11+', font=("Times New Roman", 10), fill="red")
+
+def create_board(window, board_size, solution):
+    size = board_size * length
+    width = 2 * side_margin + size
+    height = 2 * top_margin + size
+    geometry = str(width) + "x" + str(height)
+    board = Toplevel(window)
+    board.title("Solution Board")
+    board.iconphoto(False, tk.PhotoImage(file='assets/logo.png'))
+    board.geometry(geometry)
+    board.resizable(False, False)
+
+    # Creating a canvas to put all the widgets inside
+    canvas = tk.Canvas(board, width = 600, height = 360)
+    canvas.pack(fill='both', expand = True)
+
+    # Creating background image
+    # bg = tk.PhotoImage(file = 'assets/bg2.png')
+    # canvas.create_image(0, 0, image=bg, anchor = "nw")
+
+    # Load logo image in the script
+    img= (Image.open("assets/wide_logo.png"))
+    #Resize the Image using resize method
+    resized_logo= img.resize((200,33), Image.ANTIALIAS)
+    logo= ImageTk.PhotoImage(resized_logo)
+    canvas.create_image(width/2, 40, image=logo, anchor = "center")
+
+    
+    create_grid(canvas, board_size)
+
+    board.mainloop()
+
--- main_gui.py
+++ main_gui.py
@@ -1,23 +1,15 @@
 import tkinter as tk
 from PIL import Image,ImageTk
+from tkinter import messagebox
+from board_gui import create_board
 
-# Setting the start window
+# Setting the start window with title, icon & fixed size
 window = tk.Tk(className=" KENKEN")
 window.iconphoto(False, tk.PhotoImage(file='assets/logo.png'))
 window.geometry("600x360")
 window.resizable(False, False)
 
-# Creating the title of the window and displaying it on the window
-# title1 = tk.Label(window, text="Welcome To", justify=tk.CENTER, font=("Times New Roman", 24), fg="white")
-# title2 = tk.Label(window, text="Kenken Game")
-# title1.grid(row=2, column=2)
-# title2.grid(row=3, column=2)
-
-# # Creating the start button
-# start_btn = tk.Button(window, text="Start Kenken", state=tk.DISABLED)
-# start_btn.grid(row=5, column=2)
-# Create style Object
-
+# Creating a canvas to put all the widgets inside
 canvas = tk.Canvas(window, width = 600, height = 360)
 canvas.pack(fill='both', expand = True)
 
@@ -25,10 +17,10 @@ canvas.pack(fill='both', expand = True)
 bg = tk.PhotoImage(file = 'assets/bg2.png')
 canvas.create_image(0, 0, image=bg, anchor = "nw")
 
-# Adding title 
+# Adding heading
 canvas.create_text(300, 50, text = 'Welcome to', font=("Times New Roman", 24), fill="white")
 
-# Load logo in the script
+# Load logo image in the script
 img= (Image.open("assets/wide_logo.png"))
 #Resize the Image using resize method
 resized_logo= img.resize((200,33), Image.ANTIALIAS)
@@ -37,13 +29,13 @@ canvas.create_image(300, 90, image=logo, anchor = "center")
 
 # Add board size Label
 canvas.create_text(210, 170, text = 'Board Size:', font=("Times New Roman", 15), fill="white")
-#Create an Entry widget to accept User Input
+#Create an Entry widget to accept User Input for board size
 board_size_entry= tk.Entry(window, width= 10)
 board_size_entry.place(x=270, y=162)
 
 # Add algorithm Label
 canvas.create_text(210, 210, text = 'Algorithm:', font=("Times New Roman", 15), fill="white")
-#Set the Menu initially
+# Set the Menu initially
 menu= tk.StringVar()
 menu.set("Select Algorithm")
 
@@ -55,19 +47,22 @@ drop.place(x=270, y=198)
 
 def get_input():
     global board_size_entry, menu
-    board_size= board_size_entry.get()
+    board_size= int(board_size_entry.get())
     algorithm = menu.get()
     
+    # # Check that all fields are filled
+    # if not board_size or algorithm == "Select Algorithm":
+    #     messagebox.showwarning("Warning", "Please, fill all the input fields")
 
-    # Check that all fields are filled
-    if not board_size or algorithm == "Select Algorithm":
-        tk.messagebox.showwarning("Warning", "Please, fill all the input fields")
+    # # Check that min no for board size is 3
+    # elif int(board_size) < 3:
+    #     messagebox.showwarning("Warning", "The minimum number for board size is 3")
 
-    if int(board_size) < 3:
-        tk.messagebox.showwarning("Warning", "The minumem number for board size is 3")
-
-    print(board_size)
-    print(algorithm)
+    # else:
+    #     solution = 0
+    #     create_board(window, board_size, solution)
+    s = 0
+    create_board(window, board_size, s)
 
 # Creating the start button
 start_btn = tk.Button(window, text="Start Kenken", font=("Times New Roman", 10), command = get_input)
