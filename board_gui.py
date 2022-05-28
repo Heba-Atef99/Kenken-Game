f7e9b63d29aec7c83c880ddf3ba103a82f57c9d7
Heba Atef <hoba30.ha@gmail.com>
2022-05-28 19:30:46 +0200

    draw full board

--- board_gui.py
+++ board_gui.py
@@ -1,27 +1,113 @@
 import tkinter as tk
 from PIL import Image,ImageTk
-from tkinter import Toplevel, messagebox
+from tkinter import Toplevel
+import numpy as np
 
 length = 80
 side_margin = 50
 top_margin = 80
+line_thickness = 2
+font_style = "Times New Roman"
+box_top_margin = top_margin + 10
 
-def create_grid(canvas, board_size):
-    top_margin = 90
-    left_margin = side_margin
+def get_min_tuple(tuple_list):
+    return list(map(min, zip(*tuple_list)))
+
+def prepare_solution(solutions, board_size):
+    sol = np.zeros((board_size, board_size), dtype=np.int8)
+    for element in solutions:
+        values = solutions[element]
+        for i, e in enumerate(element):
+            i1 = e[0] - 1
+            i2 = e[1] - 1
+            sol[i2][i1] = values[i]
+
+    return sol
+
+def draw_group_condition(canvas, cell, op, number):
+    number = '(' + str(number) + ')'
+    op = '*' if(op == '.') else op
+    txt = number + op
+    offset = 10
+    col = cell[0] - 1
+    row = cell[1] - 1
+    r_start = box_top_margin + row * length
+    start = side_margin + col * length
+    canvas.create_text(3 + offset + start, offset - 2 + r_start, text = txt, font=(font_style, 10), fill="red")
+
+def draw_cell_bh_line(canvas, cell):
+    col = cell[0]
+    row = cell[1]
+    # bottom horizontal line
+    x1 = side_margin + (col-1) * length
+    x2 = x1 + length
+    y1 = box_top_margin + row * length
+    canvas.create_line(x1, y1, x2, y1, fill="black", width=line_thickness)
+
+def draw_cell_rv_line(canvas, cell):
+    col = cell[0]
+    row = cell[1]
+    # right vertical line
+    x1 = side_margin + col * length
+    y1 = box_top_margin + (row-1) * length
+    y2 = y1 + length
+    canvas.create_line(x1, y1, x1, y2, fill="black", width=line_thickness)
+
+def draw_groups_borders(canvas, groups, board_size):
+    for g in groups:
+        cells_list = g[0]
+        min_cell = get_min_tuple(cells_list)
+        op = g[1]
+        num = g[2]
+        draw_group_condition(canvas, min_cell, op, num)
+        if len(cells_list) == 1:
+            draw_cell_bh_line(canvas, cells_list[0])
+            draw_cell_rv_line(canvas, cells_list[0])
+        else:
+            for cell in cells_list:
+                # skip the last cell
+                if cell == (board_size, board_size):
+                    continue
+                # check if the right next cell is not in the group of the current cell, then draw vertical line
+                # and make sure that this is not a cell in the last column
+                right_next_cell = (cell[0]+1, cell[1])
+                # right_next_cell[0] += 1
+                if cell[0] != board_size and right_next_cell not in cells_list:
+                    draw_cell_rv_line(canvas, cell)
+
+                # check if the below cell is in the group of the current cell, then draw horizontal line
+                # and make sure that this is not a cell in the last row
+                below_cell = (cell[0], cell[1]+1)
+                if cell[1] != board_size and below_cell not in cells_list:
+                    draw_cell_bh_line(canvas, cell)
+
+def draw_border_grid(canvas, board_size):
+    ## draw thick four lines around the whole grid
+    box_top_margin = top_margin + 10
+    size = board_size * length
+    # top line
+    canvas.create_line(side_margin, box_top_margin, side_margin + size, box_top_margin, fill="black", width=line_thickness)
+    # bottom line
+    canvas.create_line(side_margin, size + box_top_margin, side_margin + size, size + box_top_margin, fill="black", width=line_thickness)
+    # left line
+    canvas.create_line(side_margin, box_top_margin, side_margin, size + box_top_margin, fill="black", width=line_thickness)
+    # right line 
+    canvas.create_line(size + side_margin, box_top_margin, size + side_margin, size + box_top_margin, fill="black", width=line_thickness)
+
+def create_grid(canvas, board_size, sol_arr):
     for row in range(board_size):
-        r_start = top_margin + row * length
-        r_end = top_margin + (row+1) * length
+        r_start = box_top_margin + row * length
+        r_end = box_top_margin + (row+1) * length
         for i in range(board_size):
-            start = left_margin + i * length
-            end = left_margin + (i+1) * length
-            canvas.create_rectangle(start, r_start, end, r_end, outline="black", fill="white")
+            start = side_margin + i * length
+            end = side_margin + (i+1) * length
+            canvas.create_rectangle(start, r_start, end, r_end, outline="grey", fill="white")
+            ## write numbers inside the cell
             offset = length/2
-            canvas.create_text(offset + start, offset + r_start, text = '59', font=("Times New Roman", 20), fill="black")
-            offset = 7
-            canvas.create_text(3 + offset + start, offset + r_start, text = '11+', font=("Times New Roman", 10), fill="red")
+            txt = str(sol_arr[row][i])
+            canvas.create_text(offset + start, offset + r_start, text = txt, font=(font_style, 20), fill="black")
 
-def create_board(window, board_size, solution):
+def create_board_new_window(window, board_size):
     size = board_size * length
     width = 2 * side_margin + size
     height = 2 * top_margin + size
@@ -36,19 +122,19 @@ def create_board(window, board_size, solution):
     canvas = tk.Canvas(board, width = 600, height = 360)
     canvas.pack(fill='both', expand = True)
 
-    # Creating background image
-    # bg = tk.PhotoImage(file = 'assets/bg2.png')
-    # canvas.create_image(0, 0, image=bg, anchor = "nw")
-
     # Load logo image in the script
     img= (Image.open("assets/wide_logo.png"))
     #Resize the Image using resize method
     resized_logo= img.resize((200,33), Image.ANTIALIAS)
     logo= ImageTk.PhotoImage(resized_logo)
     canvas.create_image(width/2, 40, image=logo, anchor = "center")
-
     
-    create_grid(canvas, board_size)
+    return canvas, board
 
+def solve(window, board_size, groups, solutions):
+    canvas, board = create_board_new_window(window, board_size)
+    sol_arr = prepare_solution(solutions, board_size)
+    create_grid(canvas, board_size, sol_arr)
+    draw_border_grid(canvas, board_size)
+    draw_groups_borders(canvas, groups, board_size)
     board.mainloop()
-
--- main_gui.py
+++ main_gui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from PIL import Image,ImageTk
 from tkinter import messagebox
-from board_gui import create_board
+from board_gui import solve
 
+font_style = "Times New Roman"
 # Setting the start window with title, icon & fixed size
 window = tk.Tk(className=" KENKEN")
 window.iconphoto(False, tk.PhotoImage(file='assets/logo.png'))
@@ -18,7 +19,7 @@ bg = tk.PhotoImage(file = 'assets/bg2.png')
 canvas.create_image(0, 0, image=bg, anchor = "nw")
 
 # Adding heading
-canvas.create_text(300, 50, text = 'Welcome to', font=("Times New Roman", 24), fill="white")
+canvas.create_text(300, 50, text = 'Welcome to', font=(font_style, 24), fill="white")
 
 # Load logo image in the script
 img= (Image.open("assets/wide_logo.png"))
@@ -28,13 +29,13 @@ logo= ImageTk.PhotoImage(resized_logo)
 canvas.create_image(300, 90, image=logo, anchor = "center")
 
 # Add board size Label
-canvas.create_text(210, 170, text = 'Board Size:', font=("Times New Roman", 15), fill="white")
+canvas.create_text(210, 170, text = 'Board Size:', font=(font_style, 15), fill="white")
 #Create an Entry widget to accept User Input for board size
 board_size_entry= tk.Entry(window, width= 10)
 board_size_entry.place(x=270, y=162)
 
 # Add algorithm Label
-canvas.create_text(210, 210, text = 'Algorithm:', font=("Times New Roman", 15), fill="white")
+canvas.create_text(210, 210, text = 'Algorithm:', font=(font_style, 15), fill="white")
 # Set the Menu initially
 menu= tk.StringVar()
 menu.set("Select Algorithm")
@@ -55,17 +56,18 @@ def get_input():
     #     messagebox.showwarning("Warning", "Please, fill all the input fields")
 
     # # Check that min no for board size is 3
-    # elif int(board_size) < 3:
-    #     messagebox.showwarning("Warning", "The minimum number for board size is 3")
+    # elif int(board_size) < 2:
+    #     messagebox.showwarning("Warning", "The minimum number for board size is 2")
 
     # else:
     #     solution = 0
     #     create_board(window, board_size, solution)
-    s = 0
-    create_board(window, board_size, s)
+    groups = [(((1, 1),), '.', 3), (((2, 1), (3, 1), (3, 2), (2, 2)), '+', 8), (((1, 2), (1, 3)), '-', -1), (((2, 3), (3, 3)), '-', -2)]
+    solutions = {((1, 1),): (3,), ((3, 1), (3, 2), (2, 1), (2, 2)): (1, 2, 2, 3), ((1, 2), (1, 3)): (1, 2), ((2, 3), (3, 3)): (1, 3)}
+    solve(window, board_size, groups, solutions)
 
 # Creating the start button
-start_btn = tk.Button(window, text="Start Kenken", font=("Times New Roman", 10), command = get_input)
+start_btn = tk.Button(window, text="Start Kenken", font=(font_style, 10), command = get_input)
 start_btn.place(x=262, y=280)
 
 # As of the window is in event listner mode
